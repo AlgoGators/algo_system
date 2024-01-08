e2189ea396bcc0026480453f383d9e4ca8a1e545
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-01-08 11:17:33 -0500

    adding trend signal gen wrappers

---
+++ get_trend_signals.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from trend_following import multitrend
+
+"""
+2. Calculate positions as a result of trend following (Nᵀ)
+    Using the list of instruments from step 1, generate positions using multitrend.py and return a dictionary of positions using the function trend_forecast() from trend_following/multitrend.py
+        Arguments:
+            instruments : list[str] e.g. ['ES', 'ZF']
+                list of instruments to generate positions for
+            weights : list[float] e.g. [0.5, 0.5]
+                weights for each instrument
+            capital : int
+                trading capital
+            risk_target_tau : float
+                risk target e.g. 0.20
+            multipliers : dict e.g. {'ES' : 1.0, 'ZF' : 1.0}
+                multipliers for each instrument
+            fast_spans : list[int] e.g. [20, 50]
+                fast spans for each instrument
+
+        Returns a tuple of two dictionaries, one for the buffered positions and one for the unbuffered positions
+"""
+def get_trend_positions(
+    instruments : list,
+    weights : dict,
+    capital : int,
+    risk_target_tau : float,
+    multipliers : dict,
+    fast_spans : list,
+    ) -> dict:
+
+    # Get positions from multitrend.py
+    positions = multitrend.trend_forecast(
+        instr_list=instruments,
+        weights=weights,
+        capital=capital,
+        risk_target_tau=risk_target_tau,
+        multipliers=multipliers,
+        fast_spans=fast_spans,
+        )
+
+    return positions[1]
