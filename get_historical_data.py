1ccdc5691c95406710e6be00498d72391cd29d19
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-01-25 21:33:27 -0500

    Moved to broader get_historical_data.py
    Fixed the SQL_pull function;
    Added Logging
    Added functoin to get most recent open interest

--- get_historical_data.py
+++ get_historical_data.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 import urllib
 import os
+import logging
 
-def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_column: str = 'Close'):
+def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_column: str = 'Close', interest_column : str = 'Open Interest'):
      # get all tables from database
     driver = "ODBC Driver 18 for SQL Server"
     server = os.getenv("SERVER")
@@ -11,6 +12,8 @@ def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_colum
     password = os.getenv("PASSWORD")
     database = os.getenv("DATABASE")
 
+    import time
+    start = time.time()
     # Connection string for SQL Server Authentication - do not change
     params = urllib.parse.quote_plus(fr'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
     engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
@@ -20,8 +23,8 @@ def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_colum
         table_names_query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_catalog='" + database + "'"
         table_names = pd.read_sql(table_names_query, engine)['table_name'].tolist()
     except:
-        print("Error: Unable to retrieve table names from database.")
-        return
+        # Raise the error if the table isn't found; considering it returned from the function anyway.
+        raise KeyError("Unable to retrieve table names from database.")
 
     # Convert instrument list to name in database (e.g. 'ES' -> 'ES_Data')
 
@@ -48,7 +51,7 @@ def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_colum
     # Get all adjusted close prices in each dataframe
     adjusted_prices = {}
     for instrument in instrument_list:
-        adjusted_price_df = instrument_dataframes[instrument]['Close']
+        adjusted_price_df = instrument_dataframes[instrument][adj_column]
         
         # Converted series to frame
         adjusted_price_df = adjusted_price_df.to_frame()
@@ -58,34 +61,50 @@ def SQL_pull(instrument_list: list, unadj_column: str = 'Unadj_Close', adj_colum
     # Get all unadjusted close prices in each dataframe
     current_prices = {}
     for instrument in instrument_list:
-        current_price_df = instrument_dataframes[instrument]['Unadj_Close']
+        current_price_df = instrument_dataframes[instrument][unadj_column]
 
         # Converted series to frame
         current_price_df = current_price_df.to_frame()
 
-        current_prices[instrument] = current_price_df        
+        current_prices[instrument] = current_price_df
 
-    return adjusted_prices, current_prices
+    open_interest = {}
+    for instrument in instrument_list:
+        open_interest_df = instrument_dataframes[instrument][interest_column]
+
+        open_interest_df = open_interest_df.to_frame()
+
+        open_interest[instrument] = open_interest_df
+
+    return adjusted_prices, current_prices, open_interest
 
 class Prices:
-    def get_all_historical_prices(self, instruments, price_column : str = 'Close') -> pd.DataFrame:
-        prices_df = pd.DataFrame()
+    def get_all_historical_prices(self, instruments : list[str], price_column : str = 'Close', interest_column : str = 'Open Interest') -> pd.DataFrame:
+        collective_price_df = pd.DataFrame()
+        collective_open_interest_df = pd.DataFrame()
 
-        # Grabs the adjusted prices
-        prices_dct = SQL_pull(instruments, adj_column=price_column)[0]
+        # Grabs the adjusted prices (_ is the current_prices which we dont need)
+        prices_dct, _, open_interest_dct = SQL_pull(instruments, adj_column=price_column, interest_column=interest_column)
 
-        for instrument in prices_dct.keys():
+        for instrument in instruments:
+            # Grabs the price & open interest dataframes for each instrument
             price_df : pd.DataFrame = prices_dct[instrument]
+            open_interest_df : pd.DataFrame = open_interest_dct[instrument]
 
             price_df.rename(columns={price_column : instrument}, inplace=True)
+            open_interest_df.rename(columns={interest_column : instrument}, inplace=True)
+            # print(price_df)
 
-            if prices_df.empty:
-                prices_df = prices_dct[instrument]
+            # If nothing has been added yet
+            if collective_price_df.empty:
+                collective_price_df = price_df
+                collective_open_interest_df = open_interest_df
                 continue
+            
+            collective_price_df = collective_price_df.join(price_df, how='inner')
+            collective_open_interest_df = collective_open_interest_df.join(open_interest_df, how='inner')
 
-            prices_df = prices_df.join(prices_dct[instrument], how='inner')
-
-        return prices_df
+        return collective_price_df, collective_open_interest_df
 
     def get_most_recent_prices(prices_df : pd.DataFrame) -> dict:
         most_recent_prices = {}
@@ -97,7 +116,15 @@ class Prices:
 
         return most_recent_prices
 
+def get_most_recent_open_interest(open_interest_df : pd.DataFrame) -> dict:
+    most_recent_open_interest = {}
+
+    instruments = open_interest_df.columns.tolist()
+
+    for instrument in instruments:
+        most_recent_open_interest[instrument] = open_interest_df[instrument].iloc[-1]
+
+    return most_recent_open_interest
+
 if __name__ == '__main__':
     df = Prices().get_all_historical_prices(['6A', 'ES', 'ZF'])
-
-    print(df)
