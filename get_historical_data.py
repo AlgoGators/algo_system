91abaff80b3d2113af54d27c036d643b2f512c82
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2024-01-30 13:13:26 -0500

    Update get_historical_data.py

--- get_historical_data.py
+++ get_historical_data.py
@@ -60,16 +60,16 @@ class SQLPull:
             adjusted_price_df = adjusted_price_df.to_frame()
 
             adjusted_prices[instrument] = adjusted_price_df
-            
+
         # Get all unadjusted close prices in each dataframe
-        current_prices = {}
+        unadj_prices = {}
         for instrument in instrument_list:
-            current_price_df = instrument_dataframes[instrument][unadj_column]
+            unadj_price_df = instrument_dataframes[instrument][unadj_column]
 
             # Converted series to frame
-            current_price_df = current_price_df.to_frame()
+            unadj_price_df = unadj_price_df.to_frame()
 
-            current_prices[instrument] = current_price_df
+            unadj_prices[instrument] = unadj_price_df
 
         open_interest = {}
         for instrument in instrument_list:
@@ -79,7 +79,7 @@ class SQLPull:
 
             open_interest[instrument] = open_interest_df
 
-        return adjusted_prices, current_prices, open_interest
+        return adjusted_prices, unadj_prices, open_interest
 
     def get_carry_data(instrument_list: list, carry_suffix : str = '_Data_Carry'):
         # get all tables from database
@@ -129,13 +129,13 @@ class SQLPull:
         return instrument_dataframes
 
 class Prices:
-    def get_all_historical_prices(instruments : list[str], price_column : str = 'Close', interest_column : str = 'Open Interest') -> pd.DataFrame:
+    def get_all_historical_prices(instruments : list[str], adj_column : str = 'Close', unadj_column : str = 'Unadj_Close', interest_column : str = 'Open Interest') -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         collective_adj_price_df = pd.DataFrame()
         collective_open_interest_df = pd.DataFrame()
         collective_unadj_price_df = pd.DataFrame()
 
         # Grabs the adjusted prices (_ is the current_prices which we dont need)
-        adj_prices_dct, unadj_prices_dct, open_interest_dct = SQLPull.get_price_data(instruments, adj_column=price_column, interest_column=interest_column)
+        adj_prices_dct, unadj_prices_dct, open_interest_dct = SQLPull.get_price_data(instruments, adj_column=adj_column, unadj_column=unadj_column, interest_column=interest_column)
 
         for instrument in instruments:
             # Grabs the price & open interest dataframes for each instrument
@@ -143,10 +143,9 @@ class Prices:
             unadj_price_df : pd.DataFrame = unadj_prices_dct[instrument]
             open_interest_df : pd.DataFrame = open_interest_dct[instrument]
 
-            adj_price_df.rename(columns={price_column : instrument}, inplace=True)
-            unadj_price_df.rename(columns={price_column : instrument}, inplace=True)
+            adj_price_df.rename(columns={adj_column : instrument}, inplace=True)
+            unadj_price_df.rename(columns={unadj_column : instrument}, inplace=True)
             open_interest_df.rename(columns={interest_column : instrument}, inplace=True)
-            # print(price_df)
 
             # If nothing has been added yet
             if collective_adj_price_df.empty:
