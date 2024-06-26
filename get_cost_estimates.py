de915869bfe94c27337e5b3449ec664a389e6b03
Cole Rottenberg <Cole.rottenberg@gmail.com>
2024-06-26 19:02:12 -0400

    Start from scratch

--- get_cost_estimates.py
+++
@@ -1,8 +0,0 @@
-def get_contract_cost_estimates(instruments : list, cost_estimate : float = None) -> dict:
-    """For now we will assume all costs are the same"""
-
-    contract_costs = {}
-    for instrument in instruments:
-        contract_costs[instrument] = cost_estimate
-
-    return contract_costs
